0695ee61412e8d415e0a6af3a6add554d04f2d48
Rogier de Ruijter <rogierderuijter@hotmail.com>
2023-06-16 15:31:42 +0200

    fix html strucutre

--- src/components/collapsable.js
+++ src/components/collapsable.js
@@ -9,7 +9,8 @@ export class JourneyCollapsable extends HTMLElement {
 
     const overlay = document.createElement("div");
     const button = document.createElement("button");
-    button.innerHTML = '<svg width="16" height="16" fill="currentColor"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/></svg>';
+    button.innerHTML =
+      '<svg width="16" height="16" fill="currentColor"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/></svg>';
 
     const style = document.createElement("style");
     shadowRoot.append(container, overlay, button, style);
@@ -60,12 +61,11 @@ export class JourneyCollapsable extends HTMLElement {
 
   connectedCallback() {
     this.shadowRoot.querySelector("button")?.addEventListener("click", (e) => {
-        if (this.getAttribute("open") !== "") {
-            this.setAttribute("open", "");
-        } else {
-            this.removeAttribute("open");
-        }
-      
+      if (this.getAttribute("open") !== "") {
+        this.setAttribute("open", "");
+      } else {
+        this.removeAttribute("open");
+      }
     });
   }
 }
